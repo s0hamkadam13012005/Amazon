d67554aa456acb576cb6767b17b4249c732f12b8
s0hamkadam13012005 <sohamkadam754@gmail.com>
2025-04-22 17:11:06 +0530

    generate HTML for ordersummary and delete btn interactive

--- data/cart.js
+++ data/cart.js
@@ -54,3 +54,4 @@ export function removeFromCart(productId){
     cart = newCart;
     saveToStorage();
 }
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,127 +1,112 @@
-import {cart , removeFromCart} from '../data/cart.js';
-import { formatCurrency } from './utils/money.js';
+import {cart, removeFromCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-let cartSummaryHtml = '';
+let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
-    const productId = cartItem.productId;
-  
-    let matchingProduct;
-  
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
+  const productId = cartItem.productId;
+
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+  cartSummaryHTML += `
+    <div class="cart-item-container
+      js-cart-item-container-${matchingProduct.id}">
+      <div class="delivery-date">
+        Delivery date: Tuesday, June 21
+      </div>
+
+      <div class="cart-item-details-grid">
+        <img class="product-image"
+          src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+          <div class="product-price">
+            $${formatCurrency(matchingProduct.priceCents)}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary">
+              Update
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
+          </div>
+        </div>
+
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          <div class="delivery-option">
+            <input type="radio" checked
+              class="delivery-option-input"
+              name="delivery-option-${matchingProduct.id}">
+            <div>
+              <div class="delivery-option-date">
+                Tuesday, June 21
+              </div>
+              <div class="delivery-option-price">
+                FREE Shipping
+              </div>
+            </div>
+          </div>
+          <div class="delivery-option">
+            <input type="radio"
+              class="delivery-option-input"
+              name="delivery-option-${matchingProduct.id}">
+            <div>
+              <div class="delivery-option-date">
+                Wednesday, June 15
+              </div>
+              <div class="delivery-option-price">
+                $4.99 - Shipping
+              </div>
+            </div>
+          </div>
+          <div class="delivery-option">
+            <input type="radio"
+              class="delivery-option-input"
+              name="delivery-option-${matchingProduct.id}">
+            <div>
+              <div class="delivery-option-date">
+                Monday, June 13
+              </div>
+              <div class="delivery-option-price">
+                $9.99 - Shipping
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+});
+
+document.querySelector('.js-order-summary')
+  .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
     });
-
-
-cartSummaryHtml += `
-<div class="cart-item-container   
-           js-cart-item-container-${matchingProduct.id}">
-            <div class="delivery-date">
-              Delivery date: Wednesday, June 15
-            </div>
-
-            <div class="cart-item-details-grid">
-              <img class="product-image"
-                  src="${matchingProduct.image}"
-              <div class="cart-item-details">
-                <div class="product-name">
-                    ${matchingProduct.name}
-                </div>
-                <div class="product-price">
-                    ${formatCurrency(matchingProduct.priceCents)}
-                </div>
-                <div class="product-quantity">
-                  <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-                  </span>
-                  <span class="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id = ${matchingProduct.id}>
-                    Delete
-                  </span>
-                </div>
-              </div>
-
-              <div class="delivery-options">
-                <div class="delivery-options-title">
-                  Choose a delivery option:
-                </div>
-
-                <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" checked class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>`;
-        });
-
-        document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
-
-        document.querySelectorAll('.js-delete-link').forEach((link) =>{
-            link.addEventListener('click', () =>{
-                const productId = link.dataset.productId;
-
-                removeFromCart(productId);
-
-                const container = document.querySelector(`js-cart-item-container-${productId}`);
-                container.remove();
-            })
-        })
-
-
-
-        /*
-       #1 The productId from the button → gets compared to → cartItem.productId in the cart.
-
-        🧩 Why do we match it?
-        Because we wanna know:
-
-        💡 “Is this product already in the cart?”
-
-        If yes, increase the quantity.
-
-        If no, add it as a new entry.
-                */
-
-
-        /*  #2
-        In amazon.js → you're rendering products (from your product catalog), so you use product.id.
-
-        In checkout.js → you're rendering cart items, but need full product info, so you look up each one and store it as matchingProduct, then use matchingProduct.id.
-        */
+  });
