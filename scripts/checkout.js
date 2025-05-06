77a4d0b9e2bd4d230c18d7a66c2b2285f7772ad6
s0hamkadam13012005 <sohamkadam754@gmail.com>
2025-05-06 12:21:52 +0530

    load products from backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,8 +4,8 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import { paymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-oops.js';
 //import '../data/cart-class.js';
-import '../data/car.js';
-import '../data/backend-practice.js'
+//import '../data/car.js';
+//import '../data/backend-practice.js'
 
 
  
